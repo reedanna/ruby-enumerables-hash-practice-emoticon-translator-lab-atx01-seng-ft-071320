9c0526ce8c12d1fc05aa1c538f557e9da2a88e45
Anna Nicole Reed <reedanna@mail.com>
2020-07-01 22:15:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,10 @@
 # require modules here
+require "yaml"
 
 def load_library
   # code goes here
+  library = YAML.load_file('emoticons.yml')
+  #do what i did with the pigeons to rearrange stuff here
 end
 
 def get_japanese_emoticon
