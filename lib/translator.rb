55df42f79fcea7ea0c77fc1ba4f6a5df7804a5bf
Anna Nicole Reed <reedanna@mail.com>
2020-07-01 22:32:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ end
 
 def get_english_meaning(path, emoticon)
   library = load_library(path)
-  print library.key(emoticon)
+  library.key(emoticon)
 end
 
 get_english_meaning("./lib/emoticons.yml", "(Ｔ▽Ｔ)")
